679b3f6eb9060ac1193623691ad3f0ac8e30a759
Eduardo Russo <russoedu@gmail.com>
2016-11-21 07:20:20 -0200

    [master] Removed clear console code

--- moblet/m-legiscrawler-br.js
+++ moblet/m-legiscrawler-br.js
@@ -241,15 +241,15 @@ module.exports = {
 
     router();
 
-    // CLEAR CONSOLE
-    if (typeof console._commandLineAPI !== 'undefined') {
-      console.API = console._commandLineAPI; // chrome
-    } else if (typeof console._inspectorCommandLineAPI !== 'undefined') {
-      console.API = console._inspectorCommandLineAPI; // Safari
-    } else if (typeof console.clear !== 'undefined') {
-      console.API = console;
-    }
-
-    console.API.clear();
+  // // CLEAR CONSOLE
+  // if (typeof console._commandLineAPI !== 'undefined') {
+  //   console.API = console._commandLineAPI; // chrome
+  // } else if (typeof console._inspectorCommandLineAPI !== 'undefined') {
+  //   console.API = console._inspectorCommandLineAPI; // Safari
+  // } else if (typeof console.clear !== 'undefined') {
+  //   console.API = console;
+  // }
+  //
+  // console.API.clear();
   }
 };
