89b881be19b5296736f79a0befdeefd737ebb4fa
Eduardo Russo <russoedu@gmail.com>
2017-01-15 22:31:09 -0200

    [master] New search working

--- moblet/m-legiscrawler-br.js
+++ moblet/m-legiscrawler-br.js
@@ -23,13 +23,12 @@ module.exports = {
     $http,
     $q
   ) {
-    var baseUrl = "http://api.legiscrawler.com.br/v1/";
+    var baseUrl = "http://api.legiscrawler.com.br/v1";
 
     var page = {
       LIST: 'list',
-      CATEGORY: 'category',
       LEGISLATION: 'legislation',
-      ARTICLE: 'article'
+      SEARCH: 'search'
     };
 
     var helpers = {
@@ -55,21 +54,24 @@ module.exports = {
         var height = parseInt($mFrameSize.height(), 10);
         return (height - 50) + "px";
       },
-      goToCategoryOrLegislation: function(item) {
-        if (item.link === 'category') {
-          $stateParams.detail = page.CATEGORY;
+      goTo: function(item) {
+				console.log(item);
+				// type === LIST
+				var parent = item.parent === '/' ? '' : item.parent;
+        console.log(parent);
+        if (item.type === 'LIST') {
+          $stateParams.detail = page.LIST + '&' + parent + '/' + item.slug;
+				// type === LEGISLATION
         } else {
-          $stateParams.detail = page.LEGISLATION;
+          $stateParams.detail = page.LEGISLATION + '&' + parent + '/l/' + item.slug;
         }
-        $stateParams.detail += '&' + item.category;
+        console.log($stateParams.detail);
         $state.go('pages', $stateParams);
       },
-      goToLegislation: function(legislation) {
-        $stateParams.detail = page.LEGISLATION + '&' + legislation;
-        $state.go('pages', $stateParams);
-      },
-      goToArticle: function(legislationName, article) {
-        $stateParams.detail = page.ARTICLE + '&' + legislationName + '&' + article;
+      search: function(query) {
+        var searchQuery = ($scope.parent || '') + '?search=' + query.text;
+        $stateParams.detail = page.SEARCH + '&' + searchQuery;
+        console.log($stateParams);
         $state.go('pages', $stateParams);
       }
     };
@@ -99,9 +101,12 @@ module.exports = {
     var legislationModel = {
       getLegislation: function(legislation) {
         var deferred = $q.defer();
-        $http.get(baseUrl + (legislation || ''))
+        var url = baseUrl + (legislation || '');
+        console.log(url);
+        $http.get(url)
           .then(
             function(response) {
+              console.log(response.data);
               deferred.resolve(response.data);
             },
             function(err) {
@@ -113,211 +118,45 @@ module.exports = {
       }
     };
 
-    var listController = {
+    var controller = {
       /**
-      * Show the moblet main view
-      * @param {String} category The category
+      * Put the legislation OR the list of legislations and lists in the $scope.data
+      * @param {String} category The category or null to retrieve the home of the API
       */
       showView: function(category) {
-        show = function() {
-          // Add functions to the scope
-          if (category === undefined) {
-            var categories = [];
-            for (var i = 0; i < $scope.list.length; i++) {
-              var cat = $scope.list[i].category;
-              var constRegEx = /Constituição/;
-              if (cat.match(constRegEx)) {
-                categories.unshift({
-                  category: cat,
-                  link: 'legislation'
-                });
-              } else {
-                cat = {
-                  category: cat + 's',
-                  link: 'category'
-                };
-                var newCat = true;
-                for (var j = 0; j < categories.length; j++) {
-                  if (categories[j].category === cat.category) {
-                    newCat = false;
-                  }
-                }
-                if (newCat) {
-                  categories.push(cat);
-                }
-              }
+        $scope.parent = category;
+        legislationModel.getLegislation(category)
+          .then(function(data) {
+            if (isDefined(data)) {
+              // Put the data in the $scope
+              $scope.data = data;
+              helpers.successViewLoad();
+            } else {
+              helpers.error('list not loaded');
             }
-            $scope.categories = categories;
-          } else {
-            var categoryList = [];
-            for (var k = 0; k < $scope.list.length; k++) {
-              var cate = $scope.list[k].category;
-              if (cate + 's' === category) {
-                categoryList.push($scope.list[k].name);
-              }
-            }
-            $scope.categoryList = categoryList;
-          }
-          helpers.successViewLoad();
-        };
-
-        if ($scope.list === undefined) {
-          legislationModel.getLegislation()
-            .then(function(list) {
-              if (isDefined(list) && list.length > 0) {
-                // Put the list in the $scope
-                $scope.list = list;
-
-                show();
-              } else {
-                helpers.error('list not loaded');
-              }
-            }).catch(function(err) {
+          }).catch(function(err) {
             helpers.error(err);
           });
-        } else {
-          show();
-        }
-      }
-    };
-
-    var legislationController = {
-      showView: function(legislation) {
-        $scope.isLoading = true;
-        legislationModel.getLegislation(legislation)
-          .then(function(legislation) {
-            $timeout(function() {
-              // Put the data in the $scope
-              $scope.legislation = legislation.articles;
-              $rootScope.legislation = legislation.articles;
-              $scope.legislationName = legislation.name;
-              $rootScope.legislationName = legislation.name;
-
-              helpers.successViewLoad();
-            }, 500);
-          }).catch(function(err) {
-          helpers.error(err);
-        });
-      }
-    };
-
-    var modal = {
-      // create: function() {
-      //   var template = fs.readFileSync(path.join(__dirname,
-      //     'u-wall-modal.html'), 'utf8');
-      //   $scope.modal = $ionicModal.fromTemplate(template, {
-      //     scope: $scope,
-      //     animation: 'slide-in-up',
-      //     backdropClickToClose: false,
-      //     hardwareBackButtonClose: true
-      //   // focusFirstInput: true
-      //   });
-      // },
-      close: function() {
-        $scope.modal.hide();
-      },
-      open: function() {
-        console.log($mAuth.user.get());
-        console.log($mAuth.user.get());
-        $mAuth.user.isLogged(function(logged) {
-          if (logged) {
-            $scope.modal.show();
-          } else {
-            $mAuth.setCallback(function() {
-              $mAuth.user.isLogged(function(isLogged) {
-                if (isLogged) {
-                  $timeout(function() {
-                    $ionicHistory.goBack();
-                    try {
-                      document.querySelector('textarea').style.textIndent = 0;
-                    } catch (r) {}
-                    $timeout(function() {
-                      $rootScope.$broadcast("openModalWall");
-                      $ionicLoading.hide();
-                    }, 500);
-                  }, 500);
-                } else {
-                  $timeout(function() {
-                    $ionicHistory.goBack();
-                    $timeout(function() {
-                      $rootScope.$broadcast("closeModalWall");
-                      $ionicLoading.hide();
-                    }, 500);
-                  }, 500);
-                }
-              });
-            });
-            $mAuth.login();
-          }
-        });
-      }
-    };
-
-    var articleController = {
-      addContextualActions: function() {
-        var actions = ["ion-ios-compose-outline", "ion-android-create"];
-        $mContextualActions.add(
-          $scope.page.page_id,
-          "comment",
-          actions,
-          "float",
-          modal.open
-        );
-      },
-      showView: function(legislationName, articleNumber) {
-        $scope.isLoading = true;
-        if ($rootScope.legislationName === legislationName) {
-          $timeout(function() {
-            // Put the data in the $scope
-            $scope.legislationName = legislationName;
-            $scope.article = $rootScope.legislation.filter(function(obj) {
-              return obj.number === articleNumber;
-            })[0].article;
-            // $scope.article = $rootScope.legislation[article].article;
-            $scope.number = articleNumber;
-
-            // Add the comment contextual action
-            // articleController.addContextualActions();
-
-            helpers.successViewLoad();
-          }, 500);
-        } else {
-          helpers.error();
-        }
       }
     };
 
     var router = function() {
       // Set general status
       $scope.isLoading = true;
-      $stateParams.pageTitle = null;
       appModel.loadInstanceData()
         .then(function() {
           var detail = $stateParams.detail.split('&');
           $scope.view = detail[0] === '' ? page.LIST : detail[0];
 
-          // Decide where to go based on the $stateParams
-          if ($scope.view === page.LIST) {
-            listController.showView();
-          } else if ($scope.view === page.CATEGORY) {
-            $stateParams.pageTitle = detail[1];
-            /** PRODUCT PAGE **/
-            listController.showView(detail[1]);
-          } else if ($scope.view === page.LEGISLATION) {
-            $stateParams.pageTitle = detail[1];
-            /** PRODUCT PAGE **/
-            legislationController.showView(detail[1]);
-          } else if ($scope.view === page.ARTICLE) {
-            $stateParams.pageTitle = detail[1] + ' - Art. ' + detail[2];
-            /** CATEGORY PAGE **/
-            articleController.showView(detail[1], detail[2]);
-          }
+          controller.showView(detail[1] || null);
 
           // Make the general functions avalable in the scope
           $scope.listHeight = helpers.listHeight();
-          $scope.goToLegislation = helpers.goToLegislation;
-          $scope.goToCategoryOrLegislation = helpers.goToCategoryOrLegislation;
-          $scope.goToArticle = helpers.goToArticle;
+          $scope.goTo = helpers.goTo;
+          $scope.search = helpers.search;
+          $scope.query = {
+            text: ''
+          };
         })
         .catch(function(err) {
           helpers.error(err);
